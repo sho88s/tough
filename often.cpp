cb7e3ca75c09a67af90b0dfbac19f25c99dc353d
sho88s <df93hc@outlook.com>
2024-06-27 22:53:32 +0800

    Modify often.cpp

--- often.cpp
+++ often.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# All general business call.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
