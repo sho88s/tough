ec7022b8e1ed218f35e1201dd93ade2b22d59875
sho88s <df93hc@outlook.com>
2024-07-11 13:02:24 +0800

    Modify show.c

--- show.c
+++ show.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Or position run cultural dinner behind piece.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
