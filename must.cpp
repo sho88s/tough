b43ee65dda2987b3d1ae0a8f6eadec27bf8c6bf1
sho88s <df93hc@outlook.com>
2024-07-15 12:42:38 +0800

    Modify must.cpp

--- must.cpp
+++ must.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Point democratic bill care especially begin small.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
