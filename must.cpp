3eeac1172adb41b08ccff0cd290bca0d6cd1b420
sho88s <df93hc@outlook.com>
2024-07-16 10:17:01 +0800

    Modify must.cpp

--- must.cpp
+++ must.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# From window prevent east least foot.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
