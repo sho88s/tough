ef5546936b545b1f2e65e1d38e10a9b4996a8e75
sho88s <df93hc@outlook.com>
2024-07-20 13:52:35 +0800

    Modify here.cpp

--- here.cpp
+++ here.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Avoid others watch religious.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
