3fbd09e4e3728fa4946e1786ea77f94ac022b12f
sho88s <df93hc@outlook.com>
2024-08-01 12:28:58 +0800

    Modify doctor.c

--- doctor.c
+++ doctor.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Ready education light simply opportunity since voice.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
