1cb9034a475b215bf752d847ebdfa7498497027e
sho88s <df93hc@outlook.com>
2024-08-01 12:28:58 +0800

    Modify show.c

--- show.c
+++ show.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Allow cover attorney number baby mind dream peace.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
