68b21963a92d3c6dab2bd227e030946d51c97735
sho88s <df93hc@outlook.com>
2024-08-28 15:55:37 +0800

    Modify hand.c

--- hand.c
+++ hand.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Still month cost decision before animal car.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
