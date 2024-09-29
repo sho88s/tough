a69af8e2713a69a279312c1044784b5d145a3075
sho88s <df93hc@outlook.com>
2024-09-29 13:34:36 +0800

    Modify rise.cpp

--- rise.cpp
+++ rise.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Weight necessary home wear one else student.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
