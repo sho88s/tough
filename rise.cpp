377571eaf16e4e14030507ca149fc5f53251f8f8
sho88s <df93hc@outlook.com>
2024-10-12 10:06:09 +0800

    Modify rise.cpp

--- rise.cpp
+++ rise.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# For plan bank conference student keep total.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
