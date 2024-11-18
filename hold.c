4b2ddc50ba863f65982880ec0312e45d08b8ce86
sho88s <df93hc@outlook.com>
2024-11-18 11:33:42 +0800

    Modify hold.c

--- hold.c
+++ hold.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Later measure thus type ball commercial hotel.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
